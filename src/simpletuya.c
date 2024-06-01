9f8e88d0c4aeb865a488fe12b8ce7d83205266c5
szjakub <jakubszkodny@gmail.com>
2024-06-01 13:10:04 +0200

    Add support for raw data in the DataFrame structure | change few data types

--- src/simpletuya.c
+++ src/simpletuya.c
@@ -20,295 +20,325 @@
  *
  */
 
-uint8_t TUYA_FRAME_HEADER[] = {0x55, 0xAA};
-
-
 uint8_t *u16_to_bytes(uint16_t value)
 {
-    unsigned long u16_size = sizeof(uint16_t);
-    uint8_t *bytes = (uint8_t *)malloc(u16_size);
-    for (unsigned long i=0; i<u16_size; i++)
-    {
-        bytes[i] = (value >> (8 * (u16_size - 1 - i))) & 0xFF;
-    }
-    return bytes;
+	unsigned long u16_size = sizeof(uint16_t);
+	uint8_t *bytes = (uint8_t *)malloc(u16_size);
+	for (unsigned long i = 0; i < u16_size; i++)
+	{
+		bytes[i] = (value >> (8 * (u16_size - 1 - i))) & 0xFF;
+	}
+	return bytes;
 }
 
+bool u8_in_array(uint8_t value, uint8_t *array, size_t size)
+{
+	for (int i = 0; i < size; i++)
+	{
+		if (array[i] == value)
+			return true;
+	}
+	return false;
+}
 
 uint8_t calculate_bytes_checksum(uint8_t *bytes, size_t len)
 {
-    int sum = 0;
-    for (int i=0; i<len; i++)
-        sum += bytes[i];
-    return sum % 256;
+	int sum = 0;
+	for (size_t i = 0; i < len; i++)
+		sum += bytes[i];
+	return sum % 256;
 }
-
 
 bool is_frame_valid(DataFrame *frame)
 {
-    int sum = 0;
-    sum += frame->header[0];
-    sum += frame->header[1];
-    sum += frame->version;
-    sum += frame->command;
-    sum += frame->data_len[0];
-    sum += frame->data_len[1];
+	int sum = 0;
+	uint8_t *header = decimal_to_bytes2(frame->header);
+	sum += header[0];
+	sum += header[1];
+	free(header);
 
-    DataUnit *du = frame->data_unit;
+	sum += frame->version;
+	sum += frame->command;
 
-    sum += du->dpid;
-    sum += du->type;
-    sum += du->len[0];
-    sum += du->len[1];
+	uint8_t *data_len = decimal_to_bytes2(frame->data_len);
 
-    uint8_t len_bytes[2] = {du->len[0], du->len[1]};
-    uint16_t data_len = bytes_to_decimal(uint16_t, len_bytes);
+	sum += data_len[0];
+	sum += data_len[1];
 
-    for (int i=0; i<data_len; i++)
-        sum += du->value[i];
+	DataUnit *du = frame->data_unit;
+	if (frame->data_type == DATA_UNIT)
+	{
+		sum += du->dpid;
+		sum += du->type;
+		uint8_t *len = decimal_to_bytes2(du->value_len);
 
-    return sum % 256 == frame->checksum;
+		sum += len[0];
+		sum += len[1];
+
+		for (int i = 0; i < du->value_len; i++)
+		{
+			sum += du->value[i];
+		}
+	}
+
+	return sum % 256 == frame->checksum;
 }
-
 
 char *bucket_to_str(BytesBucket *bucket)
 {
-    char *str = (char *)malloc(sizeof(uint8_t) * bucket->data_len * 5);
-    char *temp = str;
+	char *str = (char *)malloc(sizeof(uint8_t) * bucket->data_len * 5);
+	char *temp = str;
 
-    for (unsigned long i=0; i<bucket->data_len; i++)
-        temp += sprintf(temp, "%02X ", bucket->bytes[i]);
-    temp += '\0';
+	for (unsigned long i = 0; i < bucket->data_len; i++)
+		temp += sprintf(temp, "%02X ", bucket->bytes[i]);
+	temp += '\0';
 
-    return str;
+	return str;
 }
-
 
 char *frame_to_str(DataFrame *frame)
 {
-    size_t frame_size = sizeof(DataFrame) + bytes_to_decimal(uint16_t, frame->data_len);
+	char *str, *p;
 
-    char *str = (char *)malloc(sizeof(uint8_t) * frame_size * 5);
-    char *temp = str;
+	size_t frame_size = sizeof(DataFrame) + bytes_to_decimal2(frame->data_len);
+	char p = str = (char *)malloc(sizeof(uint8_t) * frame_size * 5);
 
-    temp += sprintf(temp, "%02X%02X ", frame->header[0], frame->header[1]);
-    temp += sprintf(temp, "%02X ", frame->version);
-    temp += sprintf(temp, "%02X ", frame->command);
-    temp += sprintf(temp, "%02X%02X ", frame->data_len[0], frame->data_len[1]);
+	p += sprintf(p, "%04X ", frame->header);
+	p += sprintf(p, "%02X ", frame->version);
+	p += sprintf(p, "%02X ", frame->command);
+	p += sprintf(p, "%04X ", frame->data_len);
 
-    DataUnit *du = frame->data_unit;
-    temp += sprintf(temp, "%02X ", du->dpid);
-    temp += sprintf(temp, "%02X ", du->type);
-    temp += sprintf(temp, "%02X%02X ", du->len[0], du->len[1]);
+	if (frame->data_type == DATA_UNIT)
+	{
+		DataUnit *du = frame->data_unit;
+		p += sprintf(p, "%02X ", du->dpid);
+		p += sprintf(p, "%02X ", du->type);
+		p += sprintf(p, "%04X ", du->value_len);
 
-    uint8_t *len_ptr = &du->len[0];
-    uint16_t data_len = bytes_to_decimal(uint16_t, len_ptr);
+		for (uint16_t i = 0; i < du->value_len; i++)
+		{
+			p += sprintf(p, "%02X ", du->value[i]);
+		}
+	}
+	else if (frame->data_type == RAW)
+	{
+		for (uint16_t i = 0; i < frame->data_len; i++)
+		{
+			p += sprintf(p, "%02X ", frame->raw_data[i]);
+		}
+	}
 
-    for (uint16_t i = 0; i < data_len; i++)
-    {
-        temp += sprintf(temp, "%02X ", du->value[i]);
-    }
-    temp += sprintf(temp, "%02X", frame->checksum);
-    temp += '\0';
+	p += sprintf(p, "%02X", frame->checksum);
+	p += '\0';
 
-    return str;
+	return str;
 }
-
 
 void free_data_unit(DataUnit *du)
 {
-    free(du->value);
-    free(du);
+	free(du->value);
+	free(du);
 }
-
 
 void free_data_frame(DataFrame *frame)
 {
-    if (frame->data_unit == NULL)
-        free_data_unit(frame->data_unit);
-    free(frame);
+	if (frame->data_unit == NULL)
+		free_data_unit(frame->data_unit);
+	free(frame);
 }
-
 
 void free_bytes_bucket(BytesBucket *bucket)
 {
-    free(bucket->bytes);
-    free(bucket);
+	free(bucket->bytes);
+	free(bucket);
 }
-
 
 DataUnit *bytes_to_data_unit(uint8_t *bytes, size_t len)
 {
-    if (len < 4)
-        return NULL;
+	if (len < 4)
+		return NULL;
 
-    DataUnit *du = (DataUnit *)malloc(sizeof(DataUnit));
+	DataUnit *du = (DataUnit *)malloc(sizeof(DataUnit));
 
-    du->dpid = bytes[0];
-    du->type = bytes[1];
-    du->len[0] = bytes[2];
-    du->len[1] = bytes[3];
+	du->dpid = bytes[0];
+	du->type = bytes[1];
 
-    uint8_t *len_ptr = &bytes[2];
-    uint16_t data_len = bytes_to_decimal(uint16_t, len_ptr);
+	uint8_t *len_ptr = &bytes[2];
+	du->value_len = bytes_to_decimal(uint16_t, len_ptr);
+	du->value = (uint8_t *)malloc(sizeof(uint8_t) * du->value_len);
 
-    du->value = (uint8_t *)malloc(sizeof(uint8_t));
-
-    for (size_t i=0; i<data_len; i++)
-    {
-        du->value[i] = bytes[i+4];
-    }
-    return du;
+	for (size_t i = 0; i < du->value_len; i++)
+	{
+		du->value[i] = bytes[i + 4];
+	}
+	return du;
 }
-
 
 DataFrame *bytes_to_data_frame(uint8_t *bytes, size_t len)
 {
-    if (len < 7)
-        return NULL;
+	if (len < 7)
+		return NULL;
 
-    uint8_t h1 = bytes[0];
-    uint8_t h2 = bytes[1];
-    while (h1 != TUYA_FRAME_HEADER[0] || h2 != TUYA_FRAME_HEADER[1])
-    {
-        len--;
-        if (len < 7)
-            return NULL;
+	uint8_t h1 = bytes[0];
+	uint8_t h2 = bytes[1];
+	while (h1 != TUYA_FRAME_HEADER[0] || h2 != TUYA_FRAME_HEADER[1])
+	{
+		len--;
+		if (len < 7)
+			return NULL;
 
-        bytes++;
-        h1 = bytes[0], h2 = bytes[1];
-    }
+		bytes++;
+		h1 = bytes[0], h2 = bytes[1];
+	}
 
-    DataFrame *frame = (DataFrame *)malloc(sizeof(DataFrame));
-    frame->data_unit = NULL;
+	DataFrame *frame = (DataFrame *)malloc(sizeof(DataFrame));
 
-    frame->header[0] = h1, frame->header[1] = h2;
-    frame->version = bytes[2];
-    frame->command = bytes[3];
-    frame->data_len[0] = bytes[4];
-    frame->data_len[1] = bytes[5];
+	uint8_t *header_ptr = &bytes[0];
+	frame->header = bytes_to_decimal(uint16_t, header_ptr);
 
-    uint8_t *len_ptr = &bytes[4];
-    uint16_t data_len = bytes_to_decimal(uint16_t, len_ptr);
+	frame->version = bytes[2];
+	frame->command = bytes[3];
 
-    if (len < DF_MIN_SIZE + data_len)
-    {
-        free_data_frame(frame);
-        return NULL;
-    }
+	if (u8_in_array(frame->command, DU_COMMAND_IDS, DU_COMMANDS_COUNT))
+	{
+		frame->data_type = DATA_UNIT;
+	}
+	else if (frame->data_len > 0)
+	{
+		frame->data_type = RAW;
+	}
+	else
+	{
+		frame->data_type = EMPTY;
+	}
 
-    if (data_len != 0)
-        frame->data_unit = bytes_to_data_unit(&bytes[6], data_len);
+	uint8_t *data_len_ptr = &bytes[4];
+	frame->data_len = bytes_to_decimal(uint16_t, data_len_ptr);
 
-    frame->checksum = bytes[6 + data_len];
+	if (frame->data_type == DATA_UNIT)
+	{
+		if (frame->data_len < DF_MIN_SIZE)
+		{
+			free_data_frame(frame);
+			return NULL;
+		}
+		frame->data_unit = bytes_to_data_unit(&bytes[6], frame->data_len);
+	}
+	else if (frame->data_type == RAW)
+	{
+		frame->raw_data = (uint8_t *)malloc(sizeof(uint8_t) * frame->data_len);
+		for (int i = 0; i < frame->data_len; i++)
+		{
+			frame->raw_data[i] = bytes[6 + i];
+		}
+	}
 
-    return frame;
+	frame->checksum = bytes[DF_MIN_SIZE - 1 + frame->data_len];
+	return frame;
 }
 
-
-uint8_t *data_unit_to_bytes(DataUnit *du)
+uint8_t *data_unit_to_bytes(DataUnit *data_unit)
 {
-    uint8_t *len_ptr = &du->len[0];
-    uint16_t data_len = bytes_to_decimal(uint16_t, len_ptr);
-    size_t du_size = 4 + data_len;
+	size_t du_size = DU_MIN_SIZE + data_unit->value_len;
+	uint8_t *du_bytes = (uint8_t *)malloc(sizeof(uint8_t) * du_size);
 
-    uint8_t *du_bytes = (uint8_t *)malloc(sizeof(uint8_t) * du_size);
+	du_bytes[0] = data_unit->dpid;
+	du_bytes[1] = data_unit->type;
 
-    du_bytes[0] = du->dpid;
-    du_bytes[1] = du->type;
-    du_bytes[2] = du->len[0];
-    du_bytes[3] = du->len[1];
+	uint8_t *du_len_bytes = decimal_to_bytes2(data_unit->value_len);
+	du_bytes[2] = du_len_bytes[0];
+	du_bytes[3] = du_len_bytes[1];
+	free(du_len_bytes);
 
-    memcpy(du_bytes + 4, du->value, sizeof(uint8_t) * data_len);
-
-    return du_bytes;
+	memcpy(du_bytes + 4, data_unit->value, sizeof(uint8_t) * data_unit->value_len);
+	return du_bytes;
 }
-
 
 uint8_t *data_frame_to_bytes(DataFrame *frame)
 {
-    uint8_t *len_ptr = &frame->data_len[0];
-    uint16_t data_len = bytes_to_decimal(uint16_t, len_ptr);
-    uint16_t frame_size = DF_MIN_SIZE + data_len;
+	uint8_t *header_bytes = decimal_to_bytes2(frame->header);
+	uint8_t *data_len_bytes = decimal_to_bytes2(frame->data_len);
+	uint16_t frame_size = DF_MIN_SIZE + frame->data_len;
 
-    uint8_t *frame_bytes = (uint8_t *)malloc(sizeof(uint8_t) * frame_size);
+	uint8_t *frame_bytes = (uint8_t *)malloc(sizeof(uint8_t) * frame_size);
 
-    frame_bytes[0] = frame->header[0];
-    frame_bytes[1] = frame->header[1];
-    frame_bytes[2] = frame->version;
-    frame_bytes[3] = frame->command;
-    frame_bytes[4] = frame->data_len[0];
-    frame_bytes[5] = frame->data_len[1];
+	frame_bytes[0] = header_bytes[0];
+	frame_bytes[1] = header_bytes[1];
+	frame_bytes[2] = frame->version;
+	frame_bytes[3] = frame->command;
+	frame_bytes[4] = data_len_bytes[0];
+	frame_bytes[5] = data_len_bytes[1];
 
-    uint8_t *du_bytes = data_unit_to_bytes(frame->data_unit);
-    memcpy(frame_bytes + 6, du_bytes, data_len);
+	free(data_len_bytes);
+	free(header_bytes);
 
-    free(du_bytes);
-
-    frame_bytes[6 + data_len] = frame->checksum;
-    return frame_bytes;
+	if (frame->data_type == DATA_UNIT)
+	{
+		uint8_t *du_bytes = data_unit_to_bytes(frame->data_unit);
+		memcpy(frame_bytes + 6, du_bytes, frame->data_len);
+		free(du_bytes);
+	}
+	else if (frame->data_type == RAW)
+	{
+		for (size_t i = 0; i < frame->data_len; i++)
+		{
+			frame_bytes[6 + i] = frame->raw_data[i];
+		}
+	}
+	frame_bytes[DF_MIN_SIZE - 1 + frame->data_len] = frame->checksum;
+	return frame_bytes;
 }
 
-
-BytesBucket *create_bucket(uint8_t ver, Command cmd, uint8_t dpid,
-                           DataType type, void *value, uint16_t *value_len)
+BytesBucket *create_bucket(uint8_t ver, uint8_t cmd, BucketConfig *config)
 {
-    uint16_t _value_len = 0;
-    uint8_t *value_bytes = NULL;
+	BytesBucket *bucket = (BytesBucket *)malloc(sizeof(BytesBucket));
 
-    switch (type)
-    {
-        case BOOL_T:
-        case CHAR_T:
-            _value_len = 1;
-            value_bytes = decimal_to_bytes(uint8_t, *(uint8_t *)value);
-            break;
-        case INT_T:
-            _value_len = 4;
-            value_bytes = decimal_to_bytes(int, *(int *)value);
-            break;
-        default: // for other cases length has to be specified
-            if (value_len == NULL)
-                return NULL;
-            _value_len = *value_len;
-            value_bytes = (uint8_t *)value;
-    }
+	size_t data_unit_len = 0;
+	size_t data_unit_value_len = 0;
+	uint8_t *value_bytes = NULL;
 
-    uint16_t frame_len = DF_MIN_SIZE + DU_MIN_SIZE + _value_len;
+	if (config->dpid)
+	{
+		switch (config->type)
+		{
+		case BOOL_T:
+		case CHAR_T:
+			data_unit_value_len = 1;
+			value_bytes = decimal_to_bytes(uint8_t, *(uint8_t *)config->value);
+			break;
+		case INT_T:
+			data_unit_value_len = 4;
+			value_bytes = decimal_to_bytes(int, *(int *)config->value);
+			break;
+		default: // for other cases length has to be specified
+		{
+			if (config->value_len == NULL)
+				return NULL;
+			data_unit_value_len = *(size_t *)config->value_len;
+			value_bytes = (uint8_t *)config->value;
+		}
+		}
+		data_unit_len = data_unit_value_len + DU_MIN_SIZE;
+	}
 
-    BytesBucket *bucket = (BytesBucket *)malloc(sizeof(BytesBucket));
-    uint8_t *bytes = (uint8_t *)malloc(sizeof(uint8_t) * _value_len);
+	size_t frame_len = DF_MIN_SIZE + data_unit_len;
+	uint8_t *bytes = (uint8_t *)malloc(sizeof(uint8_t) * frame_len);
 
-    bytes[0] = 0x55;
-    bytes[1] = 0xAA;
-    bytes[2] = ver;
-    bytes[3] = cmd;
+	bytes[0] = 0x55;
+	bytes[1] = 0xAA;
+	bytes[2] = ver;
+	bytes[3] = cmd;
 
-    uint16_t data_len =  DU_MIN_SIZE + _value_len;
-    uint8_t *data_len_bytes = u16_to_bytes(data_len);
-    bytes[4] = data_len_bytes[0];
-    bytes[5] = data_len_bytes[1];
-    free(data_len_bytes);
+	if (config->dpid)
+	{
+		bytes[4] = config->dpid;
+		bytes[5] = config->type;
+		memcpy(bytes + 6, config->value_len, 2);
+		memcpy(bytes + 8, value_bytes, data_unit_value_len);
+	}
+	bytes[frame_len - 1] = calculate_bytes_checksum(bytes, frame_len - 1);
 
-    bytes[6] = dpid;
-    bytes[7] = type;
-
-    uint8_t *value_len_bytes = u16_to_bytes(_value_len);
-    bytes[8] = value_len_bytes[0];
-    bytes[9] = value_len_bytes[1];
-    free(value_len_bytes);
-
-    memcpy(bytes + 10, value_bytes, _value_len);
-    bytes[10 + _value_len] = calculate_bytes_checksum(bytes, frame_len - 1);
-
-    free(value_bytes);
-
-    bucket->bytes = bytes;
-    bucket->data_len = frame_len;
-
-    return bucket;
+	bucket->bytes = bytes;
+	bucket->data_len = frame_len;
+	return bucket;
 }
-
-
-
-
--- src/simpletuya.h
+++ src/simpletuya.h
@@ -1,112 +1,135 @@
 #ifndef SIMPLETUYA_H
 #define SIMPLETUYA_H
-
 #include <stdint.h>
 #include <stdbool.h>
 
 #ifndef size_t
-typedef unsigned long size_t;
+typedef unsigned int size_t;
 #endif
-
 
 #define DF_MIN_SIZE 7
 #define DU_MIN_SIZE 4
 
+#define HEADER_SIZE 2
+#define DU_COMMANDS_COUNT 4
 
-typedef enum {
-  RAW_T     = 0x00, // N bytes
-  BOOL_T    = 0x01, // 1 bytes
-  INT_T     = 0x02, // 4 bytes
-  STR_T     = 0x03, // N bytes
-  CHAR_T    = 0x04, // 1 bytes
-  BITMAP_T  = 0x05  // 1/2/4 bytes
+const uint8_t TUYA_FRAME_HEADER[HEADER_SIZE] = {0x55, 0xAA};
+const uint8_t DU_COMMAND_IDS[DU_COMMANDS_COUNT] = {0x06, 0x07, 0x22, 0x26};
+
+typedef enum
+{
+	RAW_T = 0x00,	// N bytes
+	BOOL_T = 0x01,	// 1 bytes
+	INT_T = 0x02,	// 4 bytes
+	STR_T = 0x03,	// N bytes
+	CHAR_T = 0x04,	// 1 bytes
+	BITMAP_T = 0x05 // 1/2/4 bytes
 } DataType;
 
+typedef enum
+{
+	// module commands
+	HEALTHCHECK_C = 0x00,
+	GET_INFO_C = 0x01,
+	WORKING_MODE_C = 0x02,
+	NETWORK_STATUS_C = 0x03,
+	SEND_DATA_C = 0x06,
 
-typedef enum  {
-  // module commands
-  HBEARTBEAT_C     = 0x00,
-  GET_INFO_C       = 0x01,
-  WORKING_MODE_C   = 0x02,
-  NETWORK_STATUS_C = 0x03,
-  SEND_DATA_C      = 0x06,
-
-  // MCU commands
-  RESET_MODULE_C   = 0x04,
-  QUERY_DATA_C     = 0x07,
+	// MCU commands
+	RESET_MODULE_C = 0x04,
+	QUERY_DATA_C = 0x07,
 } Command;
 
-
-typedef struct _DataUnit
+typedef struct DataUnit
 {
-    uint8_t dpid;
-    uint8_t type;
-    uint8_t len[2];
-    uint8_t *value;
+	uint8_t dpid;
+	uint8_t type;
+	uint16_t value_len;
+	uint8_t *value;
 } DataUnit;
 
-
-typedef struct _DataFrame
+typedef struct DataFrame
 {
-    uint8_t    header[2];
-    uint8_t    version;
-    uint8_t    command;
-    uint8_t    data_len[2];
-    DataUnit  *data_unit;
-    uint8_t    checksum;
+	uint16_t header;
+	uint8_t version;
+	uint8_t command;
+	uint16_t data_len;
+	enum
+	{
+		EMPTY = 0,
+		DATA_UNIT,
+		RAW
+	} data_type;
+	union
+	{
+		DataUnit *data_unit;
+		uint8_t *raw_data;
+	};
+	uint8_t checksum;
 } DataFrame;
 
-
-typedef struct _BytesBucket
+typedef struct BucketConfig
 {
-    uint8_t *bytes;
-    size_t data_len;
+	uint8_t dpid;
+	uint8_t type;
+	void *value;
+	uint16_t *value_len;
+} BucketConfig;
+
+typedef struct BytesBucket
+{
+	uint8_t *bytes;
+	size_t data_len;
 
 } BytesBucket;
 
-
 uint8_t *u16_to_bytes(uint16_t);
-uint8_t calculate_bytes_checksum(uint8_t*, size_t);
-bool is_frame_valid(DataFrame*);
+uint8_t calculate_bytes_checksum(uint8_t *, size_t);
+bool is_frame_valid(DataFrame *);
 
-char *bucket_to_str(BytesBucket*);
-char *frame_to_str(DataFrame*);
+char *bucket_to_str(BytesBucket *);
+char *frame_to_str(DataFrame *);
 
-void free_data_frame(DataFrame*);
-void free_data_unit(DataUnit*);
-void free_bytes_bucket(BytesBucket*);
+void free_data_frame(DataFrame *);
+void free_data_unit(DataUnit *);
+void free_bytes_bucket(BytesBucket *);
 
-DataFrame *bytes_to_data_frame(uint8_t*, size_t);
-DataUnit *bytes_to_data_unit(uint8_t*, size_t);
+DataFrame *bytes_to_data_frame(uint8_t *, size_t);
+DataUnit *bytes_to_data_unit(uint8_t *, size_t);
 
-uint8_t *data_unit_to_bytes(DataUnit*);
-uint8_t *data_frame_to_bytes(DataFrame*);
-BytesBucket *create_bucket(uint8_t, Command, uint8_t, DataType, void*, uint16_t*);
+uint8_t *data_unit_to_bytes(DataUnit *);
+uint8_t *data_frame_to_bytes(DataFrame *);
+BytesBucket *create_bucket(uint8_t ver, uint8_t cmd, BucketConfig *);
 
+#define bytes_to_decimal(T, bytes)                    \
+	({                                                \
+		T value = 0;                                  \
+		size_t j = sizeof(T);                         \
+		for (unsigned long i = 0; i < sizeof(T); i++) \
+		{                                             \
+			value |= (T)bytes[--j] << (8 * i);        \
+		}                                             \
+		value;                                        \
+	})
 
-#define bytes_to_decimal(T, bytes)                                                                 \
-({                                                                                                 \
-    T value = 0;                                                                                   \
-    size_t j = sizeof(T);                                                                          \
-    for (unsigned long i = 0; i < sizeof(T); i++)                                                  \
-    {                                                                                              \
-        value |= (T)bytes[--j] << (8 * i);                                                         \
-    }                                                                                              \
-    value;                                                                                         \
-})
+#define decimal_to_bytes(T, value)                              \
+	({                                                          \
+		uint8_t *bytes = (uint8_t *)malloc(sizeof(T));          \
+		for (unsigned long i = 0; i < sizeof(T); i++)           \
+		{                                                       \
+			bytes[i] = (value >> ((sizeof(T) - 1) * 8)) & 0xFF; \
+		}                                                       \
+		bytes;                                                  \
+	})
 
-
-#define decimal_to_bytes(T, value)                                                                 \
-({                                                                                                 \
-    uint8_t *bytes = (uint8_t *)malloc(sizeof(T));                                                 \
-    for (unsigned long i=0; i<sizeof(T); i++)                                                      \
-    {                                                                                              \
-        bytes[i] = (value >> ((sizeof(T)-1) * 8)) & 0xFF;                                          \
-    }                                                                                              \
-    bytes;                                                                                         \
-})
-
+#define decimal_to_bytes2(value)                                    \
+	({                                                              \
+		uint8_t *bytes = (uint8_t *)malloc(sizeof(value));          \
+		for (unsigned long i = 0; i < sizeof(value); i++)           \
+		{                                                           \
+			bytes[i] = (value >> ((sizeof(value) - 1) * 8)) & 0xFF; \
+		}                                                           \
+		bytes;                                                      \
+	})
 
 #endif
-
-
